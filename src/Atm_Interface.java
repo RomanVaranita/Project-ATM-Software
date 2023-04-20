3552896422661f29cc446693871f88ff503bfc47
Roman Varanita <romanvaranita@gmail.com>
2023-04-20 17:30:44 +0200

    Add Class Atm_Interface

---
+++ src/Atm_Interface.java
@@ -0,0 +1,19 @@
+public class Atm_Interface {
+    public void viewBalance() {
+
+    }
+
+    public void takeAmount(double takeAmount) {
+
+    }
+
+    public void depositAmount(double depositAmount) {
+
+    }
+
+    public void viewMiniStatement() {
+
+    }
+
+
+}
