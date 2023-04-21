21c1556a64fc40bc79509da566e29c9e2e7cc883
Roman Varanita <romanvaranita@gmail.com>
2023-04-21 17:11:30 +0200

    Add Class ATM

--- src/ATM.java
+++ src/ATM.java
@@ -1,33 +1,14 @@
 public class ATM {
     private double balance;
-    private double depositAmount;
-    private double takeAmount;
 
     public ATM() {
-
+        this.balance = 2000; // Initialize the balance with 2000 euros
     }
-
     public double getBalance() {
         return balance;
     }
-
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
-    public double getDepositAmount() {
-        return depositAmount;
-    }
-
-    public void setDepositAmount(double depositAmount) {
-        this.depositAmount = depositAmount;
-    }
-
-    public double getTakeAmount() {
-        return takeAmount;
-    }
-
-    public void setTakeAmount(double takeAmount) {
-        this.takeAmount = takeAmount;
-    }
 }
+
